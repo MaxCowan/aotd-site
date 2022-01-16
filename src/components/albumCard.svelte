0a8edf9f70cd6945b5f69068a14e86c5f0a0e9a0
MaxCowan <maxace@roadrunner.com>
2022-01-16 00:12:20 -0800

    Don't use an href for the album cards yet.

--- src/components/albumCard.svelte
+++ src/components/albumCard.svelte
@@ -2,6 +2,7 @@
 export let album;
     
 </script>
-<a class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`google.com`} >
+<!-- TODO use an href when focused album view component is done -->
+<div class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" >
     <h2 class="uppercase text-2xl">{album.albumName}</h2>
-</a>
+</div>
