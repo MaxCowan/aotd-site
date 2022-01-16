69354aee21f6e89790722663e5dc65ce5330127e
MaxCowan <maxace@roadrunner.com>
2022-01-16 00:03:36 -0800

    Make a placeholder album card

---
+++ src/components/albumCard.svelte
@@ -0,0 +1,7 @@
+<script>
+export let album;
+    
+</script>
+<a class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`google.com`} >
+    <h2 class="uppercase text-2xl">{album.albumName}</h2>
+</a>
