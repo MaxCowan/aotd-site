d5819c1d217141551120db3454261f33dbee9ff9
MaxCowan <maxace@roadrunner.com>
2022-01-16 00:13:33 -0800

    Initial homepage.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import {albums, fetchSheetAlbums} from "../albumstore";
+import AlbumCard from "../components/albumCard.svelte";
+
+
+let searchTerm = "";
+let filteredAlbums = [];
+
+// React to bound searchTerm and mutate the set of albums to display
+$: {
+    if(searchTerm){
+        filteredAlbums = $albums.filter( album => album.albumName.toLowerCase().includes(searchTerm.toLowerCase()));
+    }
+    else {
+        filteredAlbums = [...$albums];
+    }
+}
+
+fetchSheetAlbums();
+
+</script>
+
+<svelte:head>
+	<title>Albums</title>
+</svelte:head>
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} placeholder="Search Albums">
+<!-- Default display grid -->
+<div class="py-4 grid gap-4 md:grid-cols-4 grid-cols-1">
+    {#each filteredAlbums as album}
+        <AlbumCard album={album}/>
+    {/each}
+</div>
