0fff213db857dcde7a7ccc4e0ac8d351178e5089
MaxCowan <maxace@roadrunner.com>
2022-01-20 09:34:08 +0000

    Reformat card preview, use a placeholder image hosted elsewhere :)

--- src/components/albumCard.svelte
+++ src/components/albumCard.svelte
@@ -3,6 +3,8 @@ export let album;
     
 </script>
 <!-- TODO use an href when focused album view component is done -->
-<div class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" >
-    <h2 class="uppercase text-2xl">{album.albumName}</h2>
+
+<div class="list-none h-[260px] w-[260px] p-6 bg-contain bg-[url('https://p1.hiclipart.com/preview/321/1024/864/itunes-13-el-capitan-apple-itunes-logo.jpg')] text-black text-center font-extrabold rounded-md shadow-md hover:shadow-2xl over:blur-sm flex flex-col items-center" >
+    <h2 class="uppercase text-2xl">{album.name}<br></h2>
+    <h2 class="uppercase text-2xl">-------<br>{album.artist}</h2>
 </div>
