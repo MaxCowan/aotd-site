713f9a627f1d9cba9dc1cd7b66f5989dc0a5770d
Max Cowan <max@DESKTOP-1AU64RB>
2024-06-20 20:42:54 -0700

    cleanup/formatting.

--- src/components/albumCard.svelte
+++ src/components/albumCard.svelte
@@ -1,11 +1,9 @@
 <script>
 export let album;
-    
 </script>
-<!-- TODO use an href when focused album view component is done -->
 
-<div class="group relative h-[260px] w-[260px] rounded-md group-hover:shadow-2xl group-hover:scale-[1.05] flex items-center transition duration-400" >
+<div class="group relative h-[260px] w-[260px] rounded-md group-hover:shadow-2xl group-hover:scale-[1.05] flex items-center transition duration-400">
     <img src="https://upload.wikimedia.org/wikipedia/en/9/9b/Tame_Impala_-_Currents.png?1642732008806" alt="" class="absolute rounded-lg drop-shadow-md w-full h-full group-hover:blur-sm group-hover:scale-[1.05] transition duration-500"/>
     <div class="z-1 absolute opacity-0 group-hover:opacity-100 group-hover:scale-[1.05] bg-gradient-to-t from-black to-transparent transition duration-500 absolute w-full h-full rounded-md"></div>
-    <div class="z-2 absolute opacity-0 group-hover:opacity-100 group-hover:-translate-y-3 transition duration-500 absolute py-2.5 bottom-0 left-4 right-4 inset-x-0 text-white text-lg text-center antialiased font-bold tracking-wide leading-6 ">{album.name}</div>
+    <div class="z-2 absolute opacity-0 group-hover:opacity-100 group-hover:-translate-y-3 transition duration-500 absolute py-2.5 bottom-0 left-4 right-4 inset-x-0 text-white text-lg text-center antialiased font-bold tracking-wide leading-6">{album.name}</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,34 +1,31 @@
 <script>
 import AlbumCard from "../components/albumCard.svelte";
-import * as albumsRawJson from '../../static/aotd_sheet_data_raw.json';
-// import * as albumsRawJson from '../../static/tester_json.json';
+import albumsRawJson from '../../static/aotd_sheet_data_raw.json';
 
-
-const albumsJson = albumsRawJson.default;
+const albumsJson = albumsRawJson;
 
 let searchTerm = "";
-let filteredAlbums = [];
+let filteredAlbums = albumsJson;
 
 // React to bound searchTerm and mutate the set of albums to display
 $: {
-    if(searchTerm){
-        filteredAlbums = albumsJson.filter( album => album.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    }
-    else {
+    if (searchTerm) {
+        filteredAlbums = albumsJson.filter(album => album.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    } else {
         filteredAlbums = albumsJson;
     }
 }
-
-filteredAlbums = albumsRawJson;
 </script>
 
 <svelte:head>
-	<title>Albums</title>
+    <title>Albums</title>
 </svelte:head>
-<input class="w-full rounded-md text-lg p-4 border-2 border-gray-500 bg-gray-100" bind:value={searchTerm} placeholder="Search Albums">
+
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-500 bg-gray-100" bind:value={searchTerm} placeholder="Search Albums" />
+
 <!-- Default display grid -->
 <div class="py-6 grid gap-5 md:grid-cols-4 grid-cols-1">
     {#each filteredAlbums as album}
-        <AlbumCard album={album}/>
+        <AlbumCard {album} />
     {/each}
 </div>
