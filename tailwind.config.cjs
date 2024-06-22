4c5ca00b083aa22a4597f6836d355ff7b4c349a5
MaxCowan <maxace@roadrunner.com>
2024-06-22 13:26:34 -0700

    install postcss-load-config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,10 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./src/**/*.svelte'],
-}
+    content: [
+      './src/**/*.{html,js,svelte,ts}'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
